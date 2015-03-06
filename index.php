d04735121b4c084c0fc3f69ab4deda6828a1f5b8
ImMrSn <misha.sn@list.ru>
2015-03-06 10:27:23 +0300

    Connetc to db
    Connect to database in index.php

--- index.php
+++ index.php
@@ -1,8 +1,53 @@
 <?php
-	// Временное решение входа.
-	if($_POST){
-		header( 'Refresh: 0; url=/home.html');
+
+	function db_connect()
+	{
+		$host = 'localhost';
+		$user = 'user';
+		$pswd = 'MxzH7wHhSXA8GqEn';
+		$db   = 'aut';
+
+		$connection = mysql_connect($host, $user, $pswd);
+
+		if (!$connection || !mysql_select_db($db, $connection))
+		{
+			return false;
+		}
+
+		return true;
 	}
+
+	function db_result_to_array($result)
+	{
+		$res_array = array();
+
+		$count = 0;
+
+		while( $row = mysql_fetch_array($result) )
+		{
+			$res_array[$count] = $row;
+			$count++;
+		}
+
+		return $res_array;
+	}
+
+	function get_students()
+	{
+		db_connect();
+
+		$query = "SELECT * FROM mem";
+
+		$result = mysql_query($query);
+
+		$result = db_result_to_array($result);
+
+		return $result;
+	}
+
+		if($_POST){
+			header( 'Refresh: 0; url=/home.html');
+		}
 ?>
 	
 <!DOCTYPE html>
@@ -14,10 +59,10 @@
 </head>
 <body>
 	<div class="login_form">
-		> <input type="text" id="flogin"> </input>
-		> <input type="password" id="fpassword"></input>
+		> <input type="text" id="flogin" name="login_in">
 		<form method="post">
 			> <input type="submit"  id="loginin" name="create" value="Вход">
+
 		</form> 
 	</div>
 </body>
